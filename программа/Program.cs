b20891f69180540a58936e8a07488643ea736c2d
saimanoon <gurylev_andrei@list.ru>
2024-03-14 23:35:24 +0300

    Добавлена функция, генерирующая массив строк, введенных пользователем

---
+++ программа/Program.cs
@@ -0,0 +1,13 @@
+﻿List<string> GenerateList()
+{
+    Random random = new Random();
+    List<string> sp = new List<string>();
+
+    for (int i = 0; i < random.Next(3, 8); i++)
+    {
+        sp.Add(Console.ReadLine());
+    }
+
+    return sp;
+}
+
